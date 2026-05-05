01e31bab5f3a984fb14bc6c38185fb7372991de6
user <email@gmail.com>
2026-05-05 12:52:15 +0300

    add eel controls for player

---
+++ main.py
@@ -0,0 +1,67 @@
+import eel
+import glob
+import os
+from player.audio_engine import AudioEngine
+
+# Создаём движок
+engine = AudioEngine()
+
+# Список песен
+songs = []
+current = -1
+
+# Загружаем песни из папки music
+def refresh_songs():
+    global songs
+    songs = sorted(glob.glob("music/*.mp3"))
+    return [os.path.basename(s) for s in songs]
+
+eel.init("web")
+
+@eel.expose
+def load_songs():
+    return refresh_songs()
+
+@eel.expose
+def play(index):
+    global current
+    if 0 <= index < len(songs):
+        current = index
+        engine.load_and_play(songs[current])
+        eel.on_play(songs[current], current + 1, len(songs))()
+
+@eel.expose
+def toggle():
+    if engine.is_playing():
+        engine.pause()
+    else:
+        engine.resume()
+    return engine.is_playing()
+
+@eel.expose
+def stop():
+    engine.stop()
+
+@eel.expose
+def next_song():
+    global current
+    if songs:
+        current = (current + 1) % len(songs)
+        engine.load_and_play(songs[current])
+        eel.on_play(songs[current], current + 1, len(songs))()
+
+@eel.expose
+def prev_song():
+    global current
+    if songs:
+        current = (current - 1) % len(songs)
+        engine.load_and_play(songs[current])
+        eel.on_play(songs[current], current + 1, len(songs))()
+
+@eel.expose
+def set_vol(v):
+    engine.set_volume(v)
+
+if __name__ == "__main__":
+    refresh_songs()
+    eel.start("web/index.html", size=(900, 600), port=0)
